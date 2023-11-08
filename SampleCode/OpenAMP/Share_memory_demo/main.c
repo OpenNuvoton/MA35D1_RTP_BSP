7db45a79cb73b55c22efc7e35e3443e8d6a9830b
Jacky Huang <ychuang3@nuvoton.com>
2023-11-08 11:42:26 +0800

    Update OpenAMP/Share_memory_demo sample code
    1. Fixed infinite RX interrupt issue. Add check to clear
       RX interrupt status, but not depend on RX ready.
    2. Add time-out check to the waiting loop for TX ACK.
    
    By the way, fix coding style.
    
    Change-Id: Ied6cfa259880d17e606edfc9dd38c842461f6507

--- SampleCode/OpenAMP/Share_memory_demo/main.c
+++ SampleCode/OpenAMP/Share_memory_demo/main.c
@@ -13,7 +13,7 @@
 
 #define M4_COMMAND_ACK 0x81
 
-static uint32_t rx_status = 0;
+static volatile uint32_t rx_status = 0;
 
 #define tx_rx_size 128
 uint8_t received_rpmsg[tx_rx_size];
@@ -21,112 +21,133 @@ uint8_t transmit_rpmsg[tx_rx_size];
 
 static int rx_callback(struct rpmsg_endpoint *rp_chnl, void *data, size_t len, uint32_t src, void *priv);
 
+volatile uint32_t  g_tick_cnt;
+
+void SysTick_Handler(void)
+{
+	g_tick_cnt++;
+}
+
+void enable_sys_tick(int ticks_per_second)
+{
+	g_tick_cnt = 0;
+	if (SysTick_Config(SystemCoreClock / ticks_per_second))
+	{
+		/* Setup SysTick Timer for 1 second interrupts  */
+		printf("Set system tick error!!\n");
+		while (1);
+	}
+}
+
+uint32_t get_ticks()
+{
+	return g_tick_cnt;
+}
+
 
 void SYS_Init(void)
 {
-    /*---------------------------------------------------------------------------------------------------------*/
-    /* Init System Clock                                                                                       */
-    /*---------------------------------------------------------------------------------------------------------*/
+	/*---------------------------------------------------------------------------------------------------------*/
+	/* Init System Clock                                                                                       */
+	/*---------------------------------------------------------------------------------------------------------*/
 
-    /* Unlock protected registers */
-    SYS_UnlockReg();
+	/* Unlock protected registers */
+	SYS_UnlockReg();
 
-    /* Enable IP clock */
-    CLK_SetModuleClock(UART16_MODULE, CLK_CLKSEL3_UART16SEL_HXT, CLK_CLKDIV3_UART16(1));
-    CLK_EnableModuleClock(UART16_MODULE);
-    CLK_EnableModuleClock(TMR2_MODULE);
-    /* To update the variable SystemCoreClock */
-    SystemCoreClockUpdate();
-    /*---------------------------------------------------------------------------------------------------------*/
-    /* Init I/O Multi-function                                                                                 */
-    /*---------------------------------------------------------------------------------------------------------*/
-    /* Set multi-function pins for Debug UART RXD and TXD */
-    SYS->GPK_MFPL &= ~(SYS_GPK_MFPL_PK2MFP_Msk | SYS_GPK_MFPL_PK3MFP_Msk);
-    SYS->GPK_MFPL |= SYS_GPK_MFPL_PK2MFP_UART16_RXD | SYS_GPK_MFPL_PK3MFP_UART16_TXD;
+	/* Enable IP clock */
+	CLK_SetModuleClock(UART16_MODULE, CLK_CLKSEL3_UART16SEL_HXT, CLK_CLKDIV3_UART16(1));
+	CLK_EnableModuleClock(UART16_MODULE);
+	CLK_EnableModuleClock(TMR2_MODULE);
+	/* To update the variable SystemCoreClock */
+	SystemCoreClockUpdate();
+	/*---------------------------------------------------------------------------------------------------------*/
+	/* Init I/O Multi-function                                                                                 */
+	/*---------------------------------------------------------------------------------------------------------*/
+	/* Set multi-function pins for Debug UART RXD and TXD */
+	SYS->GPK_MFPL &= ~(SYS_GPK_MFPL_PK2MFP_Msk | SYS_GPK_MFPL_PK3MFP_Msk);
+	SYS->GPK_MFPL |= SYS_GPK_MFPL_PK2MFP_UART16_RXD | SYS_GPK_MFPL_PK3MFP_UART16_TXD;
 
-    /* Lock protected registers */
-    SYS_LockReg();
+	/* Lock protected registers */
+	SYS_LockReg();
 }
 
 int32_t main (void)
 {
-    struct rpmsg_endpoint resmgr_ept;
-    uint32_t i;
-    int ret;
+	struct rpmsg_endpoint resmgr_ept;
+	uint32_t i, t0;
+	int ret;
 
-    /* Init System, IP clock and multi-function I/O
-       In the end of SYS_Init() will issue SYS_LockReg()
-       to lock protected register. If user want to write
-       protected register, please issue SYS_UnlockReg()
-       to unlock protected register if necessary */
-    SYS_Init();
+	/* Init System, IP clock and multi-function I/O
+	   In the end of SYS_Init() will issue SYS_LockReg()
+	   to lock protected register. If user want to write
+	   protected register, please issue SYS_UnlockReg()
+	   to unlock protected register if necessary */
+	SYS_Init();
 
-    /* Init UART to 115200-8n1 for print message */
-    UART_Open(UART16, 115200);
+	/* Init UART to 115200-8n1 for print message */
+	UART_Open(UART16, 115200);
 
-    printf("\nThis sample code demonstrate OpenAMP share memory function\n");
+	enable_sys_tick(100);
 
-    MA35D1_OpenAMP_Init(RPMSG_REMOTE, NULL);
-    OPENAMP_create_endpoint(&resmgr_ept, "rpmsg-sample", RPMSG_ADDR_ANY, rx_callback, NULL);
+	printf("\nThis sample code demonstrate OpenAMP share memory function\n");
 
-    while(1)
-    {
-        OPENAMP_check_for_message(&resmgr_ept);
+	rx_status = 0;
 
-        if(rx_status)
-        {
-            rx_status = 0;
-            break;
-        }
-    }
+	MA35D1_OpenAMP_Init(RPMSG_REMOTE, NULL);
+	OPENAMP_create_endpoint(&resmgr_ept, "rpmsg-sample", RPMSG_ADDR_ANY, rx_callback, NULL);
 
-    for(i = 0; i < tx_rx_size; i++)
-    {
-        transmit_rpmsg[i] = i;
-    }
+	while (1)
+	{
+		do {
+			OPENAMP_check_for_message(&resmgr_ept);
+		} while (!rx_status);
 
-    ret = OPENAMP_send_data(&resmgr_ept, transmit_rpmsg, 5);
-    if (ret < 0)
-    {
-        printf("Failed to send message\r\n");
-    }
+		rx_status = 0;
 
-    printf("\n Transfer %d bytes data to A35 \n", ret);
+		for (i = 0; i < tx_rx_size; i++)
+			transmit_rpmsg[i] = i;
 
-    while(1)
-    {
-        if(OPENAMP_check_TxAck(&resmgr_ept) == 1)
-        break;
-    }
+		ret = OPENAMP_send_data(&resmgr_ept, transmit_rpmsg, 5);
+		if (ret < 0)
+			printf("Failed to send message\r\n");
+		else
+			printf("\n Transfer %d bytes data to A35 \n", ret);
 
-    printf("\n Test END !!\n");
+		t0 = get_ticks();
+		while (1)
+		{
+			if (OPENAMP_check_TxAck(&resmgr_ept) == 1)
+				break;
 
-    while(1);
+			if (get_ticks() - t0 > 5)  /* 5 ms timeout */
+			{
+				printf("TX ACK time-out!\n");
+				break;
+			}
+		}
+		printf("\n Test END !!\n");
+	}
 }
 
 static int rx_callback(struct rpmsg_endpoint *rp_chnl, void *data, size_t len, uint32_t src, void *priv)
 {
-    uint32_t *u32Command = (uint32_t *)data;
-    uint32_t i;
+	uint32_t *u32Command = (uint32_t *)data;
+	uint32_t i;
 
-    if(*u32Command == COMMAND_RECEIVE_A35_MSG)
-    {
-        memcpy((void *)received_rpmsg, (const void *)src, len > sizeof(received_rpmsg) ? sizeof(received_rpmsg) : len);
+	if (*u32Command == COMMAND_RECEIVE_A35_MSG)
+	{
+		memcpy((void *)received_rpmsg, (const void *)src, len > sizeof(received_rpmsg) ? sizeof(received_rpmsg) : len);
 
-        printf("\n Receive %d bytes data from A35: \n", len);
-        for(i = 0; i < len; i++)
-        {
-            printf(" 0x%x \n", received_rpmsg[i]);
-        }
-
-        rx_status = 1;
-    }
-    else
-    {
-        printf("\n unknow command!! \n");
-    }
-
-    return 0;
+		printf("\n Receive %d bytes data from A35: \n", len);
+		for(i = 0; i < len; i++)
+		{
+			printf(" 0x%x \n", received_rpmsg[i]);
+		}
+		rx_status = 1;
+	}
+	else
+	{
+		printf("\n unknow command!! \n");
+	}
+	return 0;
 }
-
-
--- SampleCode/OpenAMP/Share_memory_demo/porting/mbox_whc.c
+++ SampleCode/OpenAMP/Share_memory_demo/porting/mbox_whc.c
@@ -10,32 +10,32 @@
 #include "NuMicro.h"
 #include "openamp_conf.h"
 
-uint32_t WHC0_RX_Flag = RX_NO_MSG;
-uint32_t WHC0_TX_Flag = TX_NO_ACK;
+volatile uint32_t WHC0_RX_Flag = RX_NO_MSG;
+volatile uint32_t WHC0_TX_Flag = TX_NO_ACK;
 
 
-//Initialize WHC
+// Initialize WHC
 int Mbox_Init(void)
 {
-    if(mbox_ch == 0)
-    {
-        WHC_ENABLE_INT(WHC0, WHC_INTEN_RX0IEN_Msk);
-        WHC_ENABLE_INT(WHC0, WHC_INTEN_TX0IEN_Msk);
-    }
-    else if(mbox_ch == 1)
-    {
-        WHC_ENABLE_INT(WHC0, WHC_INTEN_RX1IEN_Msk);
-        WHC_ENABLE_INT(WHC0, WHC_INTEN_TX1IEN_Msk);
-    }
-    else if(mbox_ch == 2)
-    {
-        WHC_ENABLE_INT(WHC0, WHC_INTEN_RX2IEN_Msk);
-        WHC_ENABLE_INT(WHC0, WHC_INTEN_TX2IEN_Msk);
-    }
+	if (mbox_ch == 0)
+	{
+		WHC_ENABLE_INT(WHC0, WHC_INTEN_RX0IEN_Msk);
+		WHC_ENABLE_INT(WHC0, WHC_INTEN_TX0IEN_Msk);
+	}
+	else if (mbox_ch == 1)
+	{
+		WHC_ENABLE_INT(WHC0, WHC_INTEN_RX1IEN_Msk);
+		WHC_ENABLE_INT(WHC0, WHC_INTEN_TX1IEN_Msk);
+	}
+	else if (mbox_ch == 2)
+	{
+		WHC_ENABLE_INT(WHC0, WHC_INTEN_RX2IEN_Msk);
+		WHC_ENABLE_INT(WHC0, WHC_INTEN_TX2IEN_Msk);
+	}
 
-    NVIC_EnableIRQ(WHC0_IRQn);
+	NVIC_EnableIRQ(WHC0_IRQn);
 
-    return 0;
+	return 0;
 }
 
 uint32_t au32RxCom[4];
@@ -47,20 +47,20 @@ uint32_t au32TxBuf[4];
 // Poll mailbox
 int Mbox_Poll(struct rpmsg_endpoint *ept)
 {
-    if (WHC0_RX_Flag == RX_NEW_MSG)
-    {
-        ept->cb(ept, &au32RxBuf, (size_t)au32RxBuf[1], SHM_START_ADDRESS, NULL);
+	if (WHC0_RX_Flag == RX_NEW_MSG)
+	{
+		ept->cb(ept, &au32RxBuf, (size_t)au32RxBuf[1], SHM_START_ADDRESS, NULL);
 
-        WHC0_RX_Flag = RX_NO_MSG;
+		WHC0_RX_Flag = RX_NO_MSG;
 
-        au32TxBuf[0] = COMMAND_RETURN_ACK; // response ack
+		au32TxBuf[0] = COMMAND_RETURN_ACK; // response ack
 
-        WHC_Send(WHC0, mbox_ch, au32TxBuf);
+		WHC_Send(WHC0, mbox_ch, au32TxBuf);
 
-        while((WHC0->TXSTS & 0xf) != 0xf); // wait Tx
+		while((WHC0->TXSTS & 0xf) != 0xf); // wait Tx
 
-        return 0;
-    }
+		return 0;
+	}
 
   return -1;
 }
@@ -68,35 +68,46 @@ int Mbox_Poll(struct rpmsg_endpoint *ept)
 // OpenAMP callback function
 int Mbox_Notify(void *priv, uint32_t id)
 {
-    return 0;
+	return 0;
 }
 
 // WHC IRQ handler
 void WHC0_IRQHandler(void)
 {
-    uint32_t i;
+	uint32_t i, ch;
 
-    if(WHC_IS_RX_READY(WHC0, mbox_ch))
-    {
-        WHC_Recv(WHC0, mbox_ch, &au32RxCom[0]);
+	for (ch = 0; ch < 4; ch++)
+	{
+		if (!(WHC0->INTSTS & (1 << (WHC_INTSTS_RX0IF_Pos + ch))))
+			continue;
 
-        if(au32RxCom[0] == COMMAND_RECEIVE_A35_MSG)
-        {
-            WHC0_RX_Flag = RX_NEW_MSG;
+		if (WHC_IS_RX_READY(WHC0, ch))
+		{
+			WHC_Recv(WHC0, ch, &au32RxCom[0]);
 
-            for(i = 0; i < 4; i++)
-                au32RxBuf[i] = au32RxCom[i];
-        }
-        else if(au32RxCom[0] == COMMAND_RECEIVE_A35_ACK)
-        {
-            WHC0_TX_Flag = TX_ACK;
-        }
+			if (au32RxCom[0] == COMMAND_RECEIVE_A35_MSG)
+			{
+				WHC0_RX_Flag = RX_NEW_MSG;
 
-        // Clear interrupt flag
-        WHC_CLR_INT_FLAG(WHC0, WHC_INTSTS_RX2IF_Msk);
-    }
+				for (i = 0; i < 4; i++)
+					au32RxBuf[i] = au32RxCom[i];
+			}
+			else if (au32RxCom[0] == COMMAND_RECEIVE_A35_ACK)
+			{
+				WHC0_TX_Flag = TX_ACK;
+			}
+		}
+		else
+		{
+			printf("CH%d RX INT without RX_READY!\n", ch);
+		}
 
-    WHC_CLR_INT_FLAG(WHC0, WHC0->TXSTS << WHC_INTSTS_TX0IF_Pos);
+		// Clear interrupt flag
+		WHC0->INTSTS = 1 << (WHC_INTSTS_RX0IF_Pos + ch);
+	}
+
+	// always clear TX interrupt status
+	WHC0->INTSTS = (WHC_INTSTS_TX0IF_Msk | WHC_INTSTS_TX1IF_Msk | WHC_INTSTS_TX2IF_Msk | WHC_INTSTS_TX3IF_Msk);
 }
 
 
